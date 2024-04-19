abe624fb31eea5d50f91f6efdce259d24dbdbf8c
Emanuele Cordano <emanuele.cordano@gmail.com>
2024-04-19 14:44:11 +0200

    enexus_hwmidcliva

--- inst/enexus_comparison/enexus_hwmidcliva.R
+++ inst/enexus_comparison/enexus_hwmidcliva.R
@@ -27,6 +27,7 @@ library(enexusClimate)
 library(raster)
 
 
+
 years <- 1983:2016
 tolerance <- 10^-5
 tmax_dataset_path <- system.file("ext_data/tmax",package="terracliva")
@@ -42,15 +43,17 @@ o_hw <- hwmidapprast(tmax_dataset_daily)
 spatial_dataset_path <- system.file("ext_data/OSM_Goma_quartiers_210527.shp",package="terracliva")    
 spatial_v <- st_as_sfc(st_bbox(tmax_dataset_daily)) %>% as_Spatial() ###spatial_dataset_path %>% st_read() 
 inputFile = brick(tmax_dataset_daily+0) ## In memory!!
+inputFile <- setZ(inputFile,time(tmax_dataset_daily))
 names(inputFile) <- format(time(tmax_dataset_daily),format="%Y.%m.%d")
+
 timerg <- range(time(tmax_dataset_daily))
 start_date <- timerg[1]
 end_date <- timerg[2]
 startDir <- '/home/ecor/local/rpackages/jrc/terracliva_material/enexus_outcomes/hwmid/goma'  
 clipByShape = TRUE                             #clip with shapefile boundaries
 ##shp_utm<-shp_create(spatial_v)
-start_year_ref <- 1981
-end_year_ref <- 2010
+start_year_ref <- year(start_date)
+end_year_ref <- year(end_date)
 
 #####
 start_date_ref <- as.Date(sprintf("%d-01-01",start_year_ref))
@@ -58,39 +61,4 @@ end_date_ref <- as.Date(sprintf("%d-12-31",end_year_ref))
 hwmi_thresh=4
 
 
-cchw <- system.time( hw <- HeatWaves(startDir=startDir, inputFile=inputFile, shp_utm=spatial_v,start_date=start_date,end_date=end_date, hwmi_thresh=hwmi_thresh, KtoC=TRUE, start_year_ref = start_year_ref,end_year_ref = end_year_ref))
-
-
-
-
-
-
-
-#' inputFile = (tmax_dataset_daily
-#' inputShape = system.file('mekrou/Mekrou_shapefile/Mekrou_AOI_v3.shp',package="enexusClimate") 
-#' dir_temp =  tmpDir()                          
-#' startDir =paste0(dir_temp,"heat_wave")         #output folder (your choice!)
-#' dir.create(startDir)
-#' start_date = as.Date('1981-01-01')       #start date
-#' end_date = as.Date('2015-12-01')         #end date
-#' clipByShape = TRUE                             #clip with shapefile boundaries
-#' shp_utm<-shp_create(inputShape)
-#' start_year_ref <- 1981
-#' end_year_ref <- 2010
-#' 
-#' #####
-#' start_date_ref <- as.Date(sprintf("%d-01-01",yref))
-#' end_date_ref <- as.Date(sprintf("%d-12-31",end_yref))
-#' hwmi_thresh=4
-#' 
-#' cchw <- system.time( hw <- HeatWaves(startDir=startDir, inputFile=inputFile, shp_utm=shp_utm,start_date=as.character(start_date),end_date=as.character(end_date), hwmi_thresh=hwmi_thresh, KtoC=TRUE, yref=yref,ndaysref=ndaysref))
-# 
-# 
-# 
-
-
-
-
-
-
-
+cchw <- system.time( hw <- HeatWaves(startDir=startDir, inputFile=inputFile, shp_utm=spatial_v,start_date=as.character(start_date),end_date=as.character(end_date), hwmi_thresh=hwmi_thresh,KtoC=TRUE,start_year_ref=start_year_ref,end_year_ref=end_year_ref))
